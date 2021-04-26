362ac492ffedd56b1f83c151885186066576600c
Filip <filip-grilec@undabot.com>
2021-04-26 19:00:45 +0200

    Added memory cache

--- HotelSearch/Controllers/WeatherForecastController.cs
+++ HotelSearch/Controllers/WeatherForecastController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
 
 namespace HotelSearch.Controllers
@@ -11,6 +12,8 @@ namespace HotelSearch.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly IMemoryCache _cache;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,22 +21,28 @@ namespace HotelSearch.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(IMemoryCache cache)
         {
-            _logger = logger;
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
         }
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-                {
-                    Date = DateTime.Now.AddDays(index),
-                    TemperatureC = rng.Next(-20, 55),
-                    Summary = Summaries[rng.Next(Summaries.Length)]
-                })
-                .ToArray();
+
+            return _cache.GetOrCreate("bla", entry =>
+            {
+                entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(10);
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                    {
+                        Date = DateTime.Now.AddDays(index),
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    })
+                    .ToArray();
+            });
+
         }
     }
 }
--- HotelSearch/Startup.cs
+++ HotelSearch/Startup.cs
@@ -31,6 +31,7 @@ namespace HotelSearch
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "HotelSearch", Version = "v1"});
             });
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
